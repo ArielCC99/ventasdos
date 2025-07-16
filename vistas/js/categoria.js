e4ea494497c42e0aca479827b947743e2e681f4c
ariel cantuna <pandarojo1999@gmail.com>
2025-07-16 00:40:49 -0500

    16/07/2025 version

--- vistas/js/categoria.js
+++ vistas/js/categoria.js
@@ -1,37 +1,61 @@
 var tabla;
 
-//Creo una función init que se ejecuta al inicio de la aplicación
-function init(){
+//creo un funcion int que se ejecuta al incio de la aplicación 
+function init() {
     mostrarform(false);
 }
+//llamamos a la funcion guardar y editar 
+$("#formulario").on("submit", function (e) {
+    guardaryeditar(e);
+});
 
 //Creo una función para limpiar el formulario
-function limpiar(){
+function limpiar() {
     $("#idcategoria").val("");
     $("#nombre").val("");
     $("#descripcion").val("");
 }
 
-//Función para mostrar el formulario
-function mostrarform(flag){
+//Función para mostrar  el formulario
+function mostrarform(flag) {
     limpiar();
-    if(flag){
-        $("#listadoregistros").hide();
+    if (flag) {
+        $("#listadoregistro").hide();
         $("#formularioregistros").show();
-        $("#btnGuardar").prop("disabled", false);
+        //$("#btnGuardar").prop("disabled", false);
         $("#btnagregar").hide();
-    }else{
+    } else {
         $("#listadoregistro").show();
         $("#formularioregistros").hide();
         $("#btnagregar").show();
     }
 }
 
-//Función para cancelar el formulario
-function cancelarform(){
+//función para cancelar el formulario
+function cancelarform() {
     limpiar();
     mostrarform(false);
 }
+//Funcion para guardar y editar los datos
+function guardaryeditar(e) {
+    //nose activa la accion predeterminada del evento
+    e.preventDefault();
+    $("#btnGuardar").prop("disabled", true);
+    var formData = new FormData($("#formulario")[0]);
 
-//Ejecutamos la función init 
+    $.ajax({
+        url: "../controladores/categoria.php?op=guardaryeditar",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (datos) {
+            bootbox.alert(datos);
+            mostrarform(false);
+            tabla.ajax.reload();
+        }
+    });
+}
+
+//ejecutamos la función init
 init();
